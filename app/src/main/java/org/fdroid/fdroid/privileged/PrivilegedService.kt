29996fb136a6bbb8fda9ef2d80677f568222293f
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-05-06 20:01:06 +0300

    Update PrivilegedService.kt

--- app/src/main/java/org/fdroid/fdroid/privileged/PrivilegedService.kt
+++ app/src/main/java/org/fdroid/fdroid/privileged/PrivilegedService.kt
@@ -171,21 +171,15 @@ class PrivilegedService : Service() {
                 IoUtils.closeQuietly(input)
                 IoUtils.closeQuietly(out)
             }
+
             val broadcastIntent = Intent(BROADCAST_ACTION_INSTALL)
             val pendingIntent = PendingIntent.getBroadcast(
-                context,
-                0,
+                this@PrivilegedService,
+                sessionId,
                 broadcastIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
             session.commit(pendingIntent.intentSender)
-            val receiver = LocalIntentReceiver()
-            Thread {
-                session.commit(receiver.getIntentSender())
-                val intent = receiver.getResult()
-                intent.action = BROADCAST_ACTION_INSTALL
-                context.sendBroadcast(intent)
-            }.start()
 
         } catch (e: IOException) {
             Log.d(TAG, "Failure", e)
